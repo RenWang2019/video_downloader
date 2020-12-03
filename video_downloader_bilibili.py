a472b460bc14b4af857ec1d4811a1757bee7e235
Ren Wang <601096493@qq.com>
2020-12-03 17:33:22 +0800

    for bilibili videos

---
+++ video_downloader_bilibili.py
@@ -0,0 +1,92 @@
+import imageio
+imageio.plugins.ffmpeg.download()
+
+import requests, time, hashlib, urllib.request, re, json
+from moviepy.editor import *
+import os, sys
+
+
+# visit API address
+def get_play_list(start_url, cid, quality):
+    entropy = 'rbMCKn@KuamXWlPMoJGsKcbiJKUfkPF_8dABscJntvqhRSETg'
+    appkey, sec = ''.join([chr(ord(i) + 2) for i in entropy[::-1]]).split(':')
+    params = 'appkey=%s&cid=%s&otype=json&qn=%s&quality=%s&type=' % (appkey, cid, quality, quality)
+    chksum = hashlib.md5(bytes(params + sec, 'utf8')).hexdigest()
+    url_api = 'https://interface.bilibili.com/v2/playurl?%s&sign=%s' % (params, chksum)
+    headers = {
+        'Referer': start_url,  # add referer
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+    }
+    # print(url_api)
+    html = requests.get(url_api, headers=headers).json()
+    # print(json.dumps(html))
+    video_list = []
+    for i in html['durl']:
+        video_list.append(i['url'])
+    # print(video_list)
+    return video_list
+
+
+# download video
+'''
+ urllib.urlretrieve callback function：
+def callbackfunc(blocknum, blocksize, totalsize):
+    @blocknum:  number of downloaded block
+    @blocksize: size of block
+    @totalsize: total size
+'''
+
+
+def Schedule_cmd(blocknum, blocksize, totalsize):
+    speed = (blocknum * blocksize) / (time.time() - start_time)
+    # speed_str = " Speed: %.2f" % speed
+    speed_str = " Speed: %s" % format_size(speed)
+    recv_size = blocknum * blocksize
+
+    # set download bars
+    f = sys.stdout
+    pervent = recv_size / totalsize
+    percent_str = "%.2f%%" % (pervent * 100)
+    n = round(pervent * 50)
+    s = ('#' * n).ljust(50, '-')
+    f.write(percent_str.ljust(8, ' ') + '[' + s + ']' + speed_str)
+    f.flush()
+    # time.sleep(0.1)
+    f.write('\r')
+
+
+def Schedule(blocknum, blocksize, totalsize):
+    speed = (blocknum * blocksize) / (time.time() - start_time)
+    # speed_str = " Speed: %.2f" % speed
+    speed_str = " Speed: %s" % format_size(speed)
+    recv_size = blocknum * blocksize
+
+    # set download bar
+    f = sys.stdout
+    pervent = recv_size / totalsize
+    percent_str = "%.2f%%" % (pervent * 100)
+    n = round(pervent * 50)
+    s = ('#' * n).ljust(50, '-')
+    print(percent_str.ljust(6, ' ') + '-' + speed_str)
+    f.flush()
+    time.sleep(2)
+    # print('\r')
+
+
+# bytes into K\M\G
+def format_size(bytes):
+    try:
+        bytes = float(bytes)
+        kb = bytes / 1024
+    except:
+        print("Wrong bytes type")
+        return "Error"
+    if kb >= 1024:
+        M = kb / 1024
+        if M >= 1024:
+            G = M / 1024
+            return "%.3fG" % (G)
+        else:
+            return "%.3fM" % (M)
+    else:
+        return "%.3fK" % (kb)
